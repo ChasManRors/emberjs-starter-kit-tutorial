82d40b1263ed2a27e5feb487da27c30877c3d26d
Charles Magid <cmagid@rbmtechnologies.com>
2014-07-20 22:04:05 -0400

    CBT not completely clear. http://emberjs.com/guides/getting-started/creating-a-new-model/

--- js/router.js
+++ js/router.js
@@ -6,6 +6,7 @@ Todos.Router.map(function() {
 // ... additional lines truncated for brevity ...
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('todo');
+    return this.store.find('todo'); 
+      // seems similar to Todo.all but that may just be because of ~/projects/starter-kit-1.6.1/index.html {{#each}} {{/each}}
   }
 });
